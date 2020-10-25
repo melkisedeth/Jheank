9b636eb372edb58fb2b13ef0bb5ea37264324642
melkisedeth <melytobias10@gmail.com>
2020-10-25 18:15:23 -0500

    jheank

---
+++ Entidad/Persona.cs
@@ -0,0 +1,16 @@
+﻿namespace Entidad
+{
+    public class Persona
+    {
+
+        public string Identificacion { get; set; }
+        public string Nombre { get; set; }
+        public string sexo { get; set; }
+        public int edad { get; set; }
+        public string departamento { get; set; }
+
+        public string ciudad { get; set; }
+
+       
+    }
+}
